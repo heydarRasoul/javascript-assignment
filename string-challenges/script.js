43665a2e4c59db522c21e1d0068a6eca7e04b34a
Heydar Rasoulpouranaei <heydarrasoulpour@ddevpipeline.com>
2025-06-26 17:37:34 -0600

    Fixing the issues highlighted in the feedback.

--- string-challenges/script.js
+++ string-challenges/script.js
@@ -12,24 +12,30 @@ console.log("=================");
 // 2. Write a program that checks if a string is blank:
 
 function isBlankString(input) {
-  return input == " " || input == "";
+  return typeof input === "string" && input.trim() === "";
 }
 console.log("challenge #2:");
 console.log(isBlankString("abc"));
 console.log(isBlankString(""));
 console.log(isBlankString(12));
 console.log(isBlankString(" "));
+console.log(isBlankString("      "));
 console.log("=================");
 
 // 3.Write a program that capitalizes the first character of a string:
 function captializeFirst(input) {
-  const firstChar = input.slice(0, 1);
-  const fisrtCharUpper = firstChar.toUpperCase();
-  const remainChars = input.slice(1);
-  return `${fisrtCharUpper}${remainChars}`;
+  if (typeof input === "string") {
+    const firstChar = input.slice(0, 1);
+    const fisrtCharUpper = firstChar.toUpperCase();
+    const remainChars = input.slice(1);
+    return `${fisrtCharUpper}${remainChars}`;
+  } else {
+    return `Oops!${input} isn't a valid input. Please enter letters only.`;
+  }
 }
 console.log("challenge #3:");
-console.log(captializeFirst("abcdef"));
+console.log(captializeFirst("abcdef3"));
+console.log(captializeFirst(1234));
 console.log("=================");
 
 // 4.Write a program that abbreviates a full name:
@@ -55,9 +61,14 @@ console.log("=================");
 
 // 6.Use slice to return from the "g" all the way to the end of the following sentence:
 
+function sliceString(string, letter) {
+  resultOfSlice = str.slice(str.indexOf(letter));
+  return resultOfSlice;
+}
+
 const str = "The practitioners grappled on the road side!";
-sliceString = str.slice(18);
 
 console.log("challenge #6:");
-console.log(sliceString);
+console.log(sliceString(str, "g"));
+
 console.log("==================");
